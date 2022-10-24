1590291fc0a9c10e6d06fbbf0ac26cdac621b022
M-Isaacson <m.isaacson@mimail.nl>
2022-10-24 16:19:36 +0000

    4th script update

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,4 @@ ssh $USER@$HOST
 cd /home/cd-example
 pip install --upgrade pip
 pip install -U -r requirements.txt
-ssh $USER@$HOST sudo systemctl restart cd-example
+ssh $USER@$HOST systemctl restart cd-example
